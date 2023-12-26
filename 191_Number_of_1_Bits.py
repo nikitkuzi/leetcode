9fd1a80273bfb549b510b10dbee3fa8af293822c
nikitkuzi <nikitkuzi@gmail.com>
2023-12-26 14:32:06 +0300

    initial commit

--- 191_Number_of_1_Bits.py
+++ 191_Number_of_1_Bits.py
@@ -1,7 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        cnt = 0
-        while n!=0:
-            cnt += (n % 2)
+        ans = 0
+        while n:
+            ans += n&1
             n >>= 1
-        return cnt
+        return ans

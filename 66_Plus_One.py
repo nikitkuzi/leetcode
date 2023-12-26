7e6141c940543ea5b8fcc4758810f01a159f89c3
nikitkuzi <nikitkuzi@gmail.com>
2023-12-26 16:56:55 +0300

    initial commit

---
+++ 66_Plus_One.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits) - 1
+        while (n >= 0 and digits[n] == 9):
+            digits[n] = 0
+            n -= 1
+        if n < 0:
+            digits.insert(0, 1)
+        if n >= 0 and digits[n] != 9:
+            digits[n] += 1
+
+        return digits

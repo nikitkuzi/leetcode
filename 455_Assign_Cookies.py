930fa9b82b2394c3fba02e486f168dd5b9387e78
nikitkuzi <nikitkuzi@gmail.com>
2024-01-21 21:14:09 +0300

    initial commit

--- 455_Assign_Cookies.py
+++ 455_Assign_Cookies.py
@@ -1,2 +1,13 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = 0
+        ans = 0
+        for cookie in s:
+            if i < len(g) and cookie >= g[i]:
+                i+=1
+                ans+=1
+            if i >= len(g) or (i < len(g) and s[-1] < g[i]):
+                break
+        return ans

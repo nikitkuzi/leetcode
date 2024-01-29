7f2bf240e059a49a6a2e174e229b92271e80a2e0
nikitkuzi <nikitkuzi@gmail.com>
2024-01-29 19:31:54 +0300

    initial commit

--- 724_Find_Pivot_Index.py
+++ 724_Find_Pivot_Index.py
@@ -2,7 +2,7 @@ class Solution(object):
     def pivotIndex(self, nums):
         leftSum, rightSum = 0, sum(nums)
         for idx, ele in enumerate(nums):
-            rightSum -= ele.
+            rightSum -= ele
             if leftSum == rightSum:
                 return idx
             leftSum += ele

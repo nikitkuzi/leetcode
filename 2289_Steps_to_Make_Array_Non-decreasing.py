7be971b54e545a56b7fa16e59594537c3d900db2
nikitkuzi <nikitkuzi@gmail.com>
2024-02-07 19:00:23 +0300

    initial commit

--- 2289_Steps_to_Make_Array_Non-decreasing.py
+++ 2289_Steps_to_Make_Array_Non-decreasing.py
@@ -1,10 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+    # def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     curr = head.next
+    #     while curr:
+    #         sm = 0    
+    #         start = curr
+    #         while start and start.val != 0:
+    #             sm += start.val
+    #             start = start.next
+    #         curr.val = sm
+    #         curr.next = start.next
+    #         curr = curr.next
+    #     return head.next
 class Solution:
-    def totalSteps(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0] * n
-        stack = []
-        for i in range(n-1, -1, -1):
-            while stack and nums[i] > nums[stack[-1]]:
-                dp[i] = max(dp[i] + 1, dp[stack.pop()])
-            stack.append(i)
-        return max(dp)
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        val = 0
+        tail = head
+        cutOff = head
+        curr = head
+        while curr :
+            val += curr.val
+            if curr.val == 0 and val > 0:
+                tail.val = val
+                cutOff = tail
+                tail = tail.next
+                val = 0
+            curr = curr.next
+        cutOff.next = None
+        return head

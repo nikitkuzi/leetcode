073b81764913ab905aae285826efc4c78264ee46
nikitkuzi <nikitkuzi@gmail.com>
2024-02-09 20:23:54 +0300

    initial commit

--- 1372_Longest_ZigZag_Path_in_a_Binary_Tree.py
+++ 1372_Longest_ZigZag_Path_in_a_Binary_Tree.py
@@ -13,3 +13,19 @@ class Solution:
 
         dfs(root, 0, 0)
         return self.mx
+
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node, left, right):
+            count = max(left, right)
+
+            if node.left:
+                count = max(count, dfs(node.left, right + 1, 0))
+
+            if node.right:
+                count = max(count, dfs(node.right, 0, left + 1))
+            return count
+
+        return dfs(root, 0, 0)

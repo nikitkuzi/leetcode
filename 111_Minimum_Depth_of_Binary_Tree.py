9193e8d0dc96093a3d4783421bb0ead429fb7500
nikitkuzi <nikitkuzi@gmail.com>
2024-02-28 20:14:38 +0300

    initial commit

---
+++ 111_Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,34 @@
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def minDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         def f(root,curr):
+#             if not root:
+#                 return float(inf)
+#             if not root.left and not root.right:
+#                 return curr
+#             return min(f(root.left,curr+1),f(root.right, curr+1))
+#         return f(root, 1)
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        queue = [(root, 1)]
+
+        while queue:
+            node, depth = queue.pop(0)
+
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))

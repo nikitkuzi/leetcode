c9c6152c3530db24b30d8b2c5fca03ad7055aca7
nikitkuzi <nikitkuzi@gmail.com>
2024-05-01 21:01:30 -0400

    initial commit

--- 3123_Find_Edges_in_Shortest_Paths.py
+++ 3123_Find_Edges_in_Shortest_Paths.py
@@ -5,22 +5,22 @@ class Solution:
             graph[u].append((v, w))
             graph[v].append((u, w))
 
-        def fn(source):
-            dist = [inf] * n
+        def djikstra(source):
+            dist = [float("inf")] * n
             dist[source] = 0
-            pq = [(0, source)]
-            while pq:
-                current_dist, node = heappop(pq)
+            q = [(0, source)]
+            while q:
+                current_dist, node = heappop(q)
                 if dist[node] == current_dist:
                     for v, w in graph[node]:
                         if current_dist + w < dist[v]:
                             dist[v] = current_dist + w
-                            heappush(pq, (current_dist + w, v))
+                            heappush(q, (current_dist + w, v))
             return dist
 
-        dist0 = fn(0)
-        dist1 = fn(n - 1)
-        if dist0[n - 1] == inf: return [False] * len(edges)
+        dist0 = djikstra(0)
+        dist1 = djikstra(n - 1)
+        if dist0[n - 1] == float("inf"): return [False] * len(edges)
         ans = []
         for u, v, w in edges:
             ans.append(dist0[u] + w + dist1[v] == dist0[n - 1] or dist0[v] + w + dist1[u] == dist0[n - 1])

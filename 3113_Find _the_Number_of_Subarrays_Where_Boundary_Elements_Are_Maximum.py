a55b808ed3676f4ca3ce7663d87b24f03ba1f710
nikitkuzi <nikitkuzi@gmail.com>
2024-05-02 21:00:19 -0400

    initial commit

---
+++ 3113_Find _the_Number_of_Subarrays_Where_Boundary_Elements_Are_Maximum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int]) -> int:
+        class Solution:
+    def numberOfSubarrays(self, nums: List[int]) -> int:
+        st = deque()
+        ans = 0
+        for x in nums:
+            while st and st[-1][0] < x:
+                st.pop()
+            if not st or st[-1][0] != x:
+                st.append((x, 1))
+            else:
+                st[-1] = (st[-1][0], st[-1][1] + 1)
+            ans += st[-1][1]
+        return ans

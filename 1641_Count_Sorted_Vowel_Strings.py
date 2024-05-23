f7417b22da00997b836eea4e54d875edaf3257bd
nikitkuzi <nikitkuzi@gmail.com>
2024-05-23 12:39:05 -0400

    initial commit

---
+++ 1641_Count_Sorted_Vowel_Strings.py
@@ -0,0 +1,39 @@
+# class Solution:
+#     def countVowelStrings(self, n: int) -> int:
+#         ans = 0
+#         def f(level,prev_max):
+#             if level == n+1:
+#                 # print(s)
+#                 nonlocal ans
+#                 ans+=1
+#                 return
+#             for i in range(prev_max, 5):
+#                 f(level+1, i)
+#         f(1,0)
+#         return ans
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        if (n == 1):
+            return 5
+        if (n == 2):
+            return 15
+        lst = [[5, 4, 3, 2, 1]]
+        lmt = [5, 15]
+
+        while (len(lmt) != n):
+            st = lst.pop()
+            ct = lmt[-1]
+            count = ct
+            tmp = [count]
+            while (st):
+                val = st.pop(0)
+                if (val <= 0):
+                    break
+                ct += count - val
+                count = count - val
+                tmp.append(count)
+
+            lst.append(tmp.copy())
+            lmt.append(ct)
+            # print(lmt,lst)
+        return lmt[-1]

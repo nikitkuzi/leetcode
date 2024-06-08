55621eeeca0c7674f539f300594d52f58f4dacf7
nikitkuzi <nikitkuzi@gmail.com>
2024-06-08 18:52:07 -0400

    initial commit

---
+++ 387_First_Unique_Character_in_a_String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        l = Counter(s)
+        for i,j in l.items():
+            if j==1:
+                return s.index(i)
+        return -1

0044f4b3cd6bc4e85c0d5bad404f97623d4d42e5
nikitkuzi <nikitkuzi@gmail.com>
2024-06-14 21:08:19 -0400

    initial commit

---
+++ 268_Missing_Number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        Tsum = (n * (n + 1)) // 2
+        actual_sum = sum(nums)
+        return Tsum - actual_sum

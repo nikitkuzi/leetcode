6452fa3ab0ad8d31b7904c5cdaf95c8c81f492d8
nikitkuzi <nikitkuzi@gmail.com>
2024-06-28 20:55:29 -0400

    initial commit

---
+++ 476_Number_Complement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        count = 0
+        temp = num
+        while temp > 0:
+            count += 1
+            temp >>= 1
+        return num ^ (2**count - 1)

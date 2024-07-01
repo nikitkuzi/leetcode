b5cd75731c58e8097d9743b684c1f1ff249ddfef
nikitkuzi <nikitkuzi@gmail.com>
2024-06-30 20:52:51 -0400

    initial commit

--- 413_Arithmetic_Slices.py
+++ 413_Arithmetic_Slices.py
@@ -1,17 +1,16 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        dp = [[0 for i in range(len(nums))] for _ in range(len(nums))]
         res = 0
-        for i in range(len(nums)-2):
-            for j in range(i+2, len(nums)):
-                # if j == i+2:
-                #     if ((nums[j+1] - nums[j]) == (nums[j+2] - nums[j+1])):
-                #         dp[i][j] = 1
-                #         res += 1
-                # else:
-                if (nums[j] - nums[j-1]) == (nums[j-1] - nums[j-2]):
-                    dp[i][j] = 1
-                    res += 1
-                if not dp[i][j]:
-                    break
+        n = len(nums)
+        if n < 2:
+            return 0
+        dp = [0]*n
+        r = 2
+        while r < n:
+            if (nums[r] - nums[r-1]) == (nums[r-1] - nums[r-2]):
+                dp[r]+=1
+                if dp[r-1]:
+                    dp[r]+=dp[r-1]
+                res += dp[r]
+            r += 1
         return res

79532dddfea7ea093a817010ce16950c4495663d
nikitkuzi <nikitkuzi@gmail.com>
2024-07-05 23:07:37 -0400

    initial commit

--- 495_Teemo_Attacking.py
+++ 495_Teemo_Attacking.py
@@ -5,4 +5,10 @@ class Solution:
         for i in range(len(A) - 1):
             ans += min(duration, A[i + 1] - A[i])
 
-        return ans + duration
+        return anclass Solution:
+    def convertToBase7(self, n: int) -> str:
+        num, s = abs(n), ''
+        while num:
+            num, remain = divmod(num,7)
+            s = str(remain) + s
+        return "-" * (n < 0) + s or "0"s + duration

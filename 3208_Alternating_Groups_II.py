5ef655da8a31d0ccbbc096e8308d21014c23a06e
nikitkuzi <nikitkuzi@gmail.com>
2024-07-06 20:56:52 -0400

    initial commit

--- 3208_Alternating_Groups_II.py
+++ 3208_Alternating_Groups_II.py
@@ -1,27 +1,14 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
         n = len(colors)
-        dp = [0] * n
+        colors += colors[:k - 1]
         res = 0
-        for i in range(1, n):
+        cnt = 1
+        for i in range(1, len(colors)):
             if colors[i] != colors[i - 1]:
-                dp[i] = dp[i - 1] + 1
-        for i in range(k - 1, n):
-            # if dp[i]-dp[i-k+1] > 0:
-            if dp[i] - k + 1 >= 0:
+                cnt += 1
+            else:
+                cnt = 1
+            if cnt >= k:
                 res += 1
-
-        # print(dp)
-        if colors[0] == colors[-1]:
-            return res
-        dp[0] = dp[-1] + 1
-        if dp[0] - k + 1 >= 0:
-            res += 1
-        for i in range(1, k - 1):
-            if dp[i]:
-                dp[i] = dp[i - 1] + 1
-            # if dp[i] - dp[-k+i+1] > 0:
-            if dp[i] - k + 1 >= 0:
-                res += 1
-        # print(dp)
         return res

322bb83ab44acd72dcffb0ff36b4328cc4876bde
nikitkuzi <nikitkuzi@gmail.com>
2024-07-24 14:04:47 -0400

    initial commit

--- 3222_Find_the_Winning_Player_in_Coin_Game.py
+++ 3222_Find_the_Winning_Player_in_Coin_Game.py
@@ -1,7 +1,7 @@
 class Solution:
     def losingPlayer(self, x: int, y: int) -> str:
         res = 0
-        while x>1 and y>4:
+        while x>=1 and y>=4:
             res ^= 1
             x-=1
             y-=4

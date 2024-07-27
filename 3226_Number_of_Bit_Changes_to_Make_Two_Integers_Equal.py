2449a47c22ed049e6ba7ca021360ba616526174a
nikitkuzi <nikitkuzi@gmail.com>
2024-07-27 10:55:08 -0400

    initial commit

--- 3226_Number_of_Bit_Changes_to_Make_Two_Integers_Equal.py
+++ 3226_Number_of_Bit_Changes_to_Make_Two_Integers_Equal.py
@@ -1,16 +1,33 @@
+# class Solution:
+#     def minChanges(self, n: int, k: int) -> int:
+#         res = 0
+#         while k:
+#             if k & 1 != n & 1:
+#                 if k&1==1:
+#                     return -1
+#                 else:
+#                     res+=1
+#             k>>=1
+#             n>>=1
+#             # print(k,n)
+#         while n:
+#             res+=int(n&1)
+#             n>>=1
+#         return res 
+
+def cardinality(bitset: int):
+    ones_count = 0
+    while bitset:
+        ones_count += (bitset & 1)
+        bitset >>= 1
+    return ones_count
+
+
 class Solution:
     def minChanges(self, n: int, k: int) -> int:
-        res = 0
-        while k:
-            if k & 1 != n & 1:
-                if k & 1 == 1:
-                    return -1
-                else:
-                    res += 1
-            k >>= 1
-            n >>= 1
-            # print(k,n)
-        while n:
-            res += int(n & 1)
-            n >>= 1
-        return res
+        a = (n ^ k)
+        b = a & n
+        if a == b:
+            return cardinality(a)
+        else:
+            return -1

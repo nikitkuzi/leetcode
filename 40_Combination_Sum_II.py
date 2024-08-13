decb2bdf0ac5b8140b98ed95c565f4e389c40fe1
nikitkuzi <nikitkuzi@gmail.com>
2024-08-12 22:22:53 -0400

    initial commit

--- 40_Combination_Sum_II.py
+++ 40_Combination_Sum_II.py
@@ -1,20 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        res = []
+        res = set()
         sm = 0
         def f(i, curr, sm):
-            print(i, curr, sm)
+            # print(i, curr, sm)
             if sm > target:
                 return
             if sm == target:
-                res.append(tuple(curr))
+                res.add(tuple(curr))
                 return
             for j in range(i+1, len(candidates)):
-                curr.append(candidates[j])
-                f(j, curr, sm+candidates[j])
-                curr.pop()
+                if j > i+1 and candidates[j] == candidates[j-1]:
+                    continue
+                # curr.append(candidates[j])
+                if candidates[j]+sm > target:
+                    break
+                f(j, curr+[candidates[j]], sm+candidates[j])
+                # curr.pop()
         for i in range(len(candidates)):
             f(i, [candidates[i]], candidates[i])
         # print(res)
-        return [list(x) for x in set(res)]
+        return res

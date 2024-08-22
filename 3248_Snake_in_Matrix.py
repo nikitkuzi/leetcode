335771c40f2bfcffec3dd68a9b15d148b7f2548a
nikitkuzi <nikitkuzi@gmail.com>
2024-08-21 21:57:05 -0400

    initial commit

--- 3248_Snake_in_Matrix.py
+++ 3248_Snake_in_Matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
-        moves = {"RIGHT":(0, 1), "LEFT":(-1, 0), "DOWN":(1, 0), "UP":(-1, 0)}
+        moves = {"RIGHT":(0, 1), "LEFT":(0, -1), "DOWN":(1, 0), "UP":(-1, 0)}
         x,y = 0, 0
         for command in commands:
             dx, dy = moves[command]

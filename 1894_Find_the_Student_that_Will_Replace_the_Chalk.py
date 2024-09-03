ce1726a9796ea1fd237783426860a39d42701067
nikitkuzi <nikitkuzi@gmail.com>
2024-09-02 20:44:55 -0400

    initial commit

--- 1894_Find_the_Student_that_Will_Replace_the_Chalk.py
+++ 1894_Find_the_Student_that_Will_Replace_the_Chalk.py
@@ -7,18 +7,22 @@ class Solution:
             pref[i] += chalk[i] + pref[i-1]
         k = k % pref[-1]
         l, r = 0, n-1
-        print(k)
-        print(pref)
-        while l < r:
-            mid = (l+r)//2
-            print(mid, pref[mid])
-            if pref[mid] >= k - pref[mid-1]:
-                r = mid -1
-            else:
-                l = mid +1
-        if l == 0 and pref[0] > k:
-            return 0
-        elif l > 0:
-            if pref[l] < k:
-                return l
-        return l+1
+        # print(k)
+        # print(pref)
+        # while l < r:
+        #     mid = (l+r)//2
+        #     print(mid, pref[mid])
+        #     if pref[mid] >= k - pref[mid-1]:
+        #         r = mid -1
+        #     else:
+        #         l = mid +1
+        # if l == 0 and pref[0] > k:
+        #     return 0
+        # elif l > 0:
+        #     if pref[l] < k:
+        #         return l
+        # return l+1
+        for i in range(n):
+            k -= chalk[i]
+            if k <= 0:
+                return i

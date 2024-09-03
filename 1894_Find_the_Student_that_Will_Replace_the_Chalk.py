2a54da0291af00960f0d635c893c7c2b390e9ff2
nikitkuzi <nikitkuzi@gmail.com>
2024-09-02 20:46:41 -0400

    initial commit

--- 1894_Find_the_Student_that_Will_Replace_the_Chalk.py
+++ 1894_Find_the_Student_that_Will_Replace_the_Chalk.py
@@ -1,28 +1,34 @@
+# class Solution:
+#     def chalkReplacer(self, chalk: List[int], k: int) -> int:
+#         n = len(chalk)
+#         pref = [0] * n
+#         pref[0] = chalk[0]
+#         for i in range(1, n):
+#             pref[i] += chalk[i] + pref[i-1]
+#         k = k % pref[-1]
+#         l, r = 0, n-1
+#         # print(k)
+#         # print(pref)
+#         # while l < r:
+#         #     mid = (l+r)//2
+#         #     print(mid, pref[mid])
+#         #     if pref[mid] >= k - pref[mid-1]:
+#         #         r = mid -1
+#         #     else:
+#         #         l = mid +1
+#         # if l == 0 and pref[0] > k:
+#         #     return 0
+#         # elif l > 0:
+#         #     if pref[l] < k:
+#         #         return l
+#         # return l+1
+#         for i in range(n):
+#             k -= chalk[i]
+#             if k < 0:
+#                 return i
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        n = len(chalk)
-        pref = [0] * n
-        pref[0] = chalk[0]
-        for i in range(1, n):
-            pref[i] += chalk[i] + pref[i-1]
-        k = k % pref[-1]
-        l, r = 0, n-1
-        # print(k)
-        # print(pref)
-        # while l < r:
-        #     mid = (l+r)//2
-        #     print(mid, pref[mid])
-        #     if pref[mid] >= k - pref[mid-1]:
-        #         r = mid -1
-        #     else:
-        #         l = mid +1
-        # if l == 0 and pref[0] > k:
-        #     return 0
-        # elif l > 0:
-        #     if pref[l] < k:
-        #         return l
-        # return l+1
-        for i in range(n):
-            k -= chalk[i]
-            if k <= 0:
-                return i
+        for i in range(1, len(chalk)):
+            chalk[i]+=chalk[i-1]
+        k%=chalk[-1]
+        return bisect.bisect_right(chalk, k)

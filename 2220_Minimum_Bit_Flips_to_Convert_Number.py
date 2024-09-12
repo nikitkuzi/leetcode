5283642aa49256c5ac557225735d20044bbc53a7
nikitkuzi <nikitkuzi@gmail.com>
2024-09-11 21:08:13 -0400

    initial commit

--- 2220_Minimum_Bit_Flips_to_Convert_Number.py
+++ 2220_Minimum_Bit_Flips_to_Convert_Number.py
@@ -1,8 +1,12 @@
+# class Solution:
+#     def minBitFlips(self, start: int, goal: int) -> int:
+#         res = 0
+#         while goal or start:
+#             res += ((start & 1) ^ (goal & 1))
+#             goal >>= 1
+#             start >>=1
+#         return res
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        res = 0
-        while goal or start:
-            res += ((start & 1) ^ (goal & 1))
-            goal >>= 1
-            start >>=1
-        return res
+        res = bin(start ^ goal)
+        return res.count('1')

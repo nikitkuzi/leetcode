030c48a734b834a3e3efa8dcd14076704d6cb500
nikitkuzi <nikitkuzi@gmail.com>
2024-09-13 21:58:03 -0400

    initial commit

--- 2419_Longest_Subarray_With_Maximum_Bitwise_AND.py
+++ 2419_Longest_Subarray_With_Maximum_Bitwise_AND.py
@@ -1,16 +1,30 @@
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         mx = 0
+#         mx_len = 0
+#         l = 0
+#         for r in range(len(nums)):
+#             if nums[r] > mx:
+#                 mx = nums[r]
+#                 mx_len = 1
+#                 l = r
+#             elif nums[r] == mx:
+#                 mx_len = max(mx_len, r-l+1)
+
+#             else:
+#                 l = r+1
+#         return mx_len
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        mx = 0
-        mx_len = 0
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] > mx:
-                mx = nums[r]
-                mx_len = 1
-                l = r
-            elif nums[r] == mx:
-                mx_len = max(mx_len, r - l + 1)
-
-            else:
-                l = r + 1
-        return mx_len
+        m = max(nums)
+        ma = 0
+        st = -1
+        for i in range(len(nums)):
+            if nums[i] == m and st == -1:
+                st = i
+            if st != -1 and nums[i] != m:
+                ma = max(i - st, ma)
+                st = -1
+        if st != -1:
+            return max(len(nums) - st, ma)
+        return ma

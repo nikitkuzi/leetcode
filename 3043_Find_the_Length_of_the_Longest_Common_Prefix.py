ebc8288aba51c578b7ca6e6e666493fc0da80082
nikitkuzi <nikitkuzi@gmail.com>
2024-09-23 20:58:50 -0400

    initial commit

--- 3043_Find_the_Length_of_the_Longest_Common_Prefix.py
+++ 3043_Find_the_Length_of_the_Longest_Common_Prefix.py
@@ -3,21 +3,25 @@ class Solution:
         trie1 = {}
         trie2 = {}
         def f(trie, arr):
-            curr = trie
             for num in arr:
+                curr = trie
                 for char in str(num):
-                    if char not in trie:
-                        trie[char] = {}
-                    trie = trie[char]
-                trie["end"] = True
+                    if char not in curr:
+                        curr[char] = {}
+                    curr = curr[char]
+                curr["end"] = True
+
         f(trie1, arr1)
         f(trie2, arr2)
+
         def f2(target_trie, source):
             mx = 0
             for num in source:
                 curr = 0
                 curr_trie = target_trie
+                print(num)
                 for char in str(num):
+                    print(char)
                     if char in curr_trie:
                         curr+=1
                         curr_trie = curr_trie[char]
@@ -25,6 +29,7 @@ class Solution:
                     else:
                         curr = 0
                         break
+                print("end")
             return mx
         mx1 = f2(trie2, arr1)
         mx2 = f2(trie1, arr2)

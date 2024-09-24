6a207a6deb7e7752bc4194b39f0423fb218b0e59
nikitkuzi <nikitkuzi@gmail.com>
2024-09-23 20:59:40 -0400

    initial commit

--- 3043_Find_the_Length_of_the_Longest_Common_Prefix.py
+++ 3043_Find_the_Length_of_the_Longest_Common_Prefix.py
@@ -1,35 +1,55 @@
+# class Solution:
+#     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+#         trie1 = {}
+#         trie2 = {}
+#         def f(trie, arr):
+#             for num in arr:
+#                 curr = trie
+#                 for char in str(num):
+#                     if char not in curr:
+#                         curr[char] = {}
+#                     curr = curr[char]
+#                 curr["end"] = True
+
+#         f(trie1, arr1)
+#         f(trie2, arr2)
+
+#         def f2(target_trie, source):
+#             mx = 0
+#             for num in source:
+#                 curr = 0
+#                 curr_trie = target_trie
+#                 print(num)
+#                 for char in str(num):
+#                     print(char)
+#                     if char in curr_trie:
+#                         curr+=1
+#                         curr_trie = curr_trie[char]
+#                         mx = max(curr, mx)
+#                     else:
+#                         curr = 0
+#                         break
+#                 print("end")
+#             return mx
+#         mx1 = f2(trie2, arr1)
+#         mx2 = f2(trie1, arr2)
+#         return max(mx1, mx2)
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        trie1 = {}
-        trie2 = {}
-        def f(trie, arr):
-            for num in arr:
-                curr = trie
-                for char in str(num):
-                    if char not in curr:
-                        curr[char] = {}
-                    curr = curr[char]
-                curr["end"] = True
+        prefixes = set()
+        for e in arr2:
+            while e > 0:
+                prefixes.add(e)
+                e //= 10
 
-        f(trie1, arr1)
-        f(trie2, arr2)
-
-        def f2(target_trie, source):
-            mx = 0
-            for num in source:
-                curr = 0
-                curr_trie = target_trie
-                print(num)
-                for char in str(num):
-                    print(char)
-                    if char in curr_trie:
-                        curr+=1
-                        curr_trie = curr_trie[char]
-                        mx = max(curr, mx)
-                    else:
-                        break
-                print("end")
-            return mx
-        mx1 = f2(trie2, arr1)
-        mx2 = f2(trie1, arr2)
-        return max(mx1, mx2)
+        res = 0
+        for e in arr1:
+            while e > res:
+                if e in prefixes:
+                    res = max(res, e)
+                    break
+                else:
+                    e //= 10
+        if res == 0:
+            return 0
+        return len(str(res))

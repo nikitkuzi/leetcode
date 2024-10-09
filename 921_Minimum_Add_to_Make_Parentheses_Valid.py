846f865b8284eafca77ac3fe37e9ebfd1423f1be
nikitkuzi <nikitkuzi@gmail.com>
2024-10-08 21:03:35 -0400

    initial commit

--- 921_Minimum_Add_to_Make_Parentheses_Valid.py
+++ 921_Minimum_Add_to_Make_Parentheses_Valid.py
@@ -2,9 +2,13 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         op = 0
         cl = 0
+        res = 0
         for c in s:
             if c == "(":
                 op +=1
             else:
-                cl+=1
-        return abs(op-cl)
+                op -= 1
+            if op < 0:
+                res +=1
+                op = 0
+        return abs(op)+res

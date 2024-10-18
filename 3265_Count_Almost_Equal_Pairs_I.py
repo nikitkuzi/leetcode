05cb13f0a4a8ac94eee3b2f36b71c41deb1dfc78
nikitkuzi <nikitkuzi@gmail.com>
2024-10-18 14:56:16 -0400

    initial commit

--- 3265_Count_Almost_Equal_Pairs_I.py
+++ 3265_Count_Almost_Equal_Pairs_I.py
@@ -1,32 +1,29 @@
 class Solution:
-	def countPairs(self, nums: List[int]) -> int:
+    def countPairs(self, nums: List[int]) -> int:
 
-		result = 0
+        result = 0
 
-		for i in range(len(nums) - 1):
+        for i in range(len(nums) - 1):
 
-			for j in range(i + 1 , len(nums)):
+            for j in range(i + 1, len(nums)):
 
-				n1 = str(nums[i])
-				n2 = str(nums[j])
+                n1 = str(nums[i])
+                n2 = str(nums[j])
 
-				if len(n1) < len(n2):
+                if len(n1) < len(n2):
+                    n1 = '0' * (len(n2) - len(n1)) + n1
 
-					n1 = '0' * (len(n2) - len(n1)) + n1
+                if len(n1) > len(n2):
+                    n2 = '0' * (len(n1) - len(n2)) + n2
 
-				if len(n1) > len(n2):
+                count = 0
 
-					n2 = '0' * (len(n1) - len(n2)) + n2
+                for index in range(len(n2)):
 
-				count = 0
+                    if n1[index] != n2[index]:
+                        count = count + 1
 
-				for index in range(len(n2)):
+                if count <= 2 and Counter(n1) == Counter(n2):
+                    result = result + 1
 
-					if n1[index] != n2[index]:
-						count = count + 1
-
-				if count <= 2 and Counter(n1) == Counter(n2):
-
-					result = result + 1
-
-		return result
+        return result

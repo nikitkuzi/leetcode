a54d324b1841b2c8b0d0e60bf171bc5ebc97aedd
nikitkuzi <nikitkuzi@gmail.com>
2024-10-25 21:01:11 -0400

    initial commit

--- 1233_Remove_Sub-Folders_from_the_Filesystem.py
+++ 1233_Remove_Sub-Folders_from_the_Filesystem.py
@@ -1,16 +1,41 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        result = []
+        prev = ''
+        for path in folder:
+            if not prev or not path.startswith(prev + '/'):
+                result.append(path)
+                prev = path  # Update 'prev' to the current folder
+        return result
+# # class Solution:
+# #     def removeSubfolders(self, folder: List[str]) -> List[str]:
+# #         trie = {}
+# #         for path in folder:
+# #             curr = trie
+# #             f = True
+# #             for c in path.split('/'):
+# #                 if c not in curr:
+# #                     curr[c] = {}
+# #                 curr = curr[c]
+# #             curr["#"] = "#"
+# #         res = 0
+# #         ans = []
+# #         for path in folder:
+# #             curr = trie
+# #             f = True
+# #             for c in path.split('/'):
+# #                 if '#' in curr:
+# #                     f = False
+# #                     break
+# #                 curr = curr[c]
+# #             if f:
+# #                 ans.append(path)
+# #         return ans
 # class Solution:
 #     def removeSubfolders(self, folder: List[str]) -> List[str]:
 #         trie = {}
-#         for path in folder:
-#             curr = trie
-#             f = True
-#             for c in path.split('/'):
-#                 if c not in curr:
-#                     curr[c] = {}
-#                 curr = curr[c]
-#             curr["#"] = "#"
-#         res = 0
-#         ans = []
+#         test = []
 #         for path in folder:
 #             curr = trie
 #             f = True
@@ -18,37 +43,22 @@
 #                 if '#' in curr:
 #                     f = False
 #                     break
+#                 if c not in curr:
+#                     curr[c] = {}
+#                 curr = curr[c]
+#             if f:
+#                 curr["#"] = "#"
+#                 test.append(path)
+#         res = 0
+#         ans = []
+#         for path in test:
+#             curr = trie
+#             f = True
+#             for c in path.split("/"):
+#                 if '#' in curr:
+#                     f = False
+#                     break
 #                 curr = curr[c]
 #             if f:
 #                 ans.append(path)
 #         return ans
-class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        trie = {}
-        test = []
-        for path in folder:
-            curr = trie
-            f = True
-            for c in path.split('/'):
-                if '#' in curr:
-                    f = False
-                    break
-                if c not in curr:
-                    curr[c] = {}
-                curr = curr[c]
-            if f:
-                curr["#"] = "#"
-                test.append(path)
-        res = 0
-        ans = []
-        for path in test:
-            curr = trie
-            f = True
-            for c in path.split("/"):
-                if '#' in curr:
-                    f = False
-                    break
-                curr = curr[c]
-            if f:
-                ans.append(path)
-        return ans

cc25d601a181b4ab4a6f141f1def12dd05558a53
nikitkuzi <nikitkuzi@gmail.com>
2024-10-25 21:00:12 -0400

    initial commit

--- 1233_Remove_Sub-Folders_from_the_Filesystem.py
+++ 1233_Remove_Sub-Folders_from_the_Filesystem.py
@@ -1,20 +1,50 @@
+# class Solution:
+#     def removeSubfolders(self, folder: List[str]) -> List[str]:
+#         trie = {}
+#         for path in folder:
+#             curr = trie
+#             f = True
+#             for c in path.split('/'):
+#                 if c not in curr:
+#                     curr[c] = {}
+#                 curr = curr[c]
+#             curr["#"] = "#"
+#         res = 0
+#         ans = []
+#         for path in folder:
+#             curr = trie
+#             f = True
+#             for c in path.split('/'):
+#                 if '#' in curr:
+#                     f = False
+#                     break
+#                 curr = curr[c]
+#             if f:
+#                 ans.append(path)
+#         return ans
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         trie = {}
+        test = []
         for path in folder:
             curr = trie
             f = True
             for c in path.split('/'):
+                if '#' in curr:
+                    f = False
+                    break
                 if c not in curr:
                     curr[c] = {}
                 curr = curr[c]
-            curr["#"] = "#"
+            if f:
+                curr["#"] = "#"
+                test.append(path)
         res = 0
         ans = []
-        for path in folder:
+        for path in test:
             curr = trie
             f = True
-            for c in path.split('/'):
+            for c in path.split("/"):
                 if '#' in curr:
                     f = False
                     break

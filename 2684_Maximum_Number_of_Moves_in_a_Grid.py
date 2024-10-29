8037d3e8ef49187b7ae80a09582f703f58187f0a
nikitkuzi <nikitkuzi@gmail.com>
2024-10-28 21:09:52 -0400

    initial commit

---
+++ 2684_Maximum_Number_of_Moves_in_a_Grid.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        moves = [[-1, 1], [0, 1], [1,1]]
+        n = len(grid)
+        m = len(grid[0])
+        dp = [[0 for i in range(m)] for i in range(n)]
+        def check(x, y):
+            return x >= 0 and x< n and y >= 0 and y < m
+        for y in range(m-2, -1, -1):
+            for x in range(n-1, -1, -1):
+                curr = 0
+                for dx, dy in moves:
+                    if check(x+dx, y+dy) and grid[x][y] < grid[x+dx][y+dy]:
+                        curr = max(dp[x+dx][y+dy]+1, curr)
+                dp[x][y] = curr
+        # for d in dp:
+            # print(d)
+        return max([dp[x][0] for x in range(n)])

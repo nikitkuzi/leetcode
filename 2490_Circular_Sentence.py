a3de9f8a77b290a841930ad431163832ea1e0a6a
nikitkuzi <nikitkuzi@gmail.com>
2024-11-03 21:44:48 -0500

    initial commit

--- 2490_Circular_Sentence.py
+++ 2490_Circular_Sentence.py
@@ -1,7 +1,7 @@
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
         words = sentence.split()
-        if words[0][0] != words[-1][-1]:
+        if words[0][0] !=words[-1][-1]:
             return False
         for i in range(len(words)-1):
             if words[i][-1] != words[i+1][0]:

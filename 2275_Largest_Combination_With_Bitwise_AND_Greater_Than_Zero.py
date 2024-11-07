14e2094d7a9d6bea2016ea9ea2928ba779b65ba5
nikitkuzi <nikitkuzi@gmail.com>
2024-11-06 20:24:34 -0500

    initial commit

--- 2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.py
+++ 2275_Largest_Combination_With_Bitwise_AND_Greater_Than_Zero.py
@@ -1,12 +1,21 @@
+import math
+
+
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        bits = [0]*24
-        def f(num):
-            i = 0
-            while num:
-                bits[23-i] += num & 1
-                num >>=1
-                i+=1
-        for n in candidates:
-            f(n)
-        return max(bits)
+        largest_value = max(candidates)
+        best_count = 0
+        largest_power = ceil(math.log(largest_value, 2))
+
+        for i in range(largest_power + 1):
+            cur_count = 0
+
+            for candidate in candidates:
+                if candidate & (1 << i):
+                    cur_count += 1
+
+            best_count = max(best_count, cur_count)
+            if best_count == largest_power:
+                return best_count
+
+        return best_count

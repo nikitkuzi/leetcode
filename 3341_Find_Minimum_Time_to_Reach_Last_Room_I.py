80d63033f183fdd2a7df7ba866da20ce1d11b80b
nikitkuzi <nikitkuzi@gmail.com>
2024-11-07 20:19:31 -0500

    initial commit

---
+++ 3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n = len(moveTime)
+        m = len(moveTime[0])
+        dp = [[0 for i in range(m)] for i in range(n)]
+        dp[0][0] = moveTime[0][0]
+        for j in range(1, m):
+            dp[0][j] = max(dp[0][j-1]+1, moveTime[0][j]+1)
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0]+1, moveTime[i][0]+1)
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = min(max(dp[i-1][j]+1, moveTime[i][j]+1), max(dp[i][j-1]+1, moveTime[i][j])+1)
+        return dp[-1][-1]

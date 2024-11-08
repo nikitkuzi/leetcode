4808e22a8e3bfbb80c291a7a5535cdacbc4069d4
nikitkuzi <nikitkuzi@gmail.com>
2024-11-07 20:21:33 -0500

    initial commit

--- 3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
+++ 3341_Find_Minimum_Time_to_Reach_Last_Room_I.py
@@ -10,5 +10,7 @@ class Solution:
             dp[i][0] = max(dp[i-1][0]+1, moveTime[i][0]+1)
         for i in range(1, n):
             for j in range(1, m):
-                dp[i][j] = min(max(dp[i-1][j]+1, moveTime[i][j]+1), max(dp[i][j-1]+1, moveTime[i][j])+1)
+                dp[i][j] = min(max(dp[i-1][j]+1, moveTime[i][j]+1), max(dp[i][j-1]+1, moveTime[i][j]+1))
+        # for d in dp:
+            # print(d)
         return dp[-1][-1]

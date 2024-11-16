9018274c7223dd18c7aced6d897c9b9abad6158a
nikitkuzi <nikitkuzi@gmail.com>
2024-11-15 23:13:48 -0500

    initial commit

--- 3255_Find_the_Power_of_K-Size_Subarrays_II.py
+++ 3255_Find_the_Power_of_K-Size_Subarrays_II.py
@@ -1,41 +1,14 @@
-# class Solution:
-#     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-# if k == 1:
-# return nums
-#         n = len(nums)
-#         pref = [0] * (n + 1)
-#         ascending = [1] * (n)
-#         res = []
-#         # pref[0] = nums[0]
-#         # print(nums)
-#         for i in range(1, n + 1):
-#             pref[i] = pref[i - 1] + nums[i - 1]
-#             if i < n:
-#                 ascending[i] = ascending[i-1]+1 if nums[i]- nums[i-1]==1 else 1
-#         # print(pref)
-#         # print(ascending)
-#         for i in range(k, n + 1):
-#             sm = pref[i] - pref[i - k]
-#             arifmetic = (nums[i-1] + nums[i-k])/2 * k
-#             # print(sm, ascending[i-1], arifmetic)
-#             if ascending[i-1] >=k and sm == arifmetic:
-#                 res.append(nums[i-1])
-#             else:
-#                 res.append(-1)
-
-
-#         return res
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        if k == 1: return nums[:]
-        n = len(nums)
-        res = [-1] * (n - k + 1)
-        i = 0
-        while i < n:
-            j = i + 1
-            while j < n and nums[j] == nums[j - 1] + 1:
-                j += 1
-                if j - i >= k: res[j - k] = nums[j - 1]
-            i = j
-        #
+        if k == 1:
+            return nums
+        cumulative_sum = 1
+        res = [-1 ] * (len(nums) - k + 1)
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                cumulative_sum += 1
+                if cumulative_sum >= k:
+                    res[i - k + 1]=nums[i]
+            else:
+                cumulative_sum = 1
         return res

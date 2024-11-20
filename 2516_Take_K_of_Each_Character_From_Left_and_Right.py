0d8109b00146acec5595bd76841f203f98396a7b
nikitkuzi <nikitkuzi@gmail.com>
2024-11-19 21:53:57 -0500

    initial commit

--- 2516_Take_K_of_Each_Character_From_Left_and_Right.py
+++ 2516_Take_K_of_Each_Character_From_Left_and_Right.py
@@ -1,26 +1,13 @@
 class Solution:
-    def takeCharacters(self, s: str, k: int) -> int:
-        c = Counter(s)
-        if k == 0:
-            return 0
-        if 'a' not in c or 'b' not in c or 'c' not in c:
-            return -1
-        if c['a'] < k or c['b'] < k or c['c'] < k:
-            return -1
-
-        def f(s, counter):
-            if not s:
-                return inf
-            if counter['a'] >= k and counter['b'] >= k and counter['c'] >= k:
-                return 0
-            res = inf
-            counter[s[0]] += 1
-            res = min(res, f(s[1:], counter) + 1)
-            counter[s[0]] -= 1
-            counter[s[-1]] += 1
-            res = min(res, f(s[:-1], counter) + 1)
-            counter[s[-1]] -= 1
-            return res
-
-        counter = defaultdict(int)
-        return f(s, counter)
+    def takeCharacters(self,s,k):
+        d={c:s.count(c)-k for c in'abc'}
+        if min(d.values())<0:
+            return-1
+        h,m,l={c:0 for c in'abc'},0,0
+        for r,c in enumerate(s):
+            h[c]+=1
+            while h[c]>d[c]:
+                h[s[l]]-=1
+                l+=1
+            m=max(m,r-l+1)
+        return len(s)-m

626de8af24106083dff1dcb47c59b4b95b57fb54
nikitkuzi <nikitkuzi@gmail.com>
2024-11-30 20:43:22 -0500

    initial commit

---
+++ 3360_Stone_Removal_Game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canAliceWin(self, n: int) -> bool:
+        sub = 10
+        alice = False
+        while n-sub>0:
+            n-=sub
+            sub-=1
+            alice^=1
+        return True if alice else False

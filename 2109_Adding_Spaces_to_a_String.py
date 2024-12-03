f7cfb51e264c19325bd3226de58df10d560f002f
nikitkuzi <nikitkuzi@gmail.com>
2024-12-02 21:30:38 -0500

    initial commit

---
+++ 2109_Adding_Spaces_to_a_String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        res = [s[:spaces[0]]]
+        for i in range(len(spaces)-1):
+            res.append(s[spaces[i]:spaces[i+1]])
+        res.append(s[spaces[-1]:])
+        return " ".join(res)

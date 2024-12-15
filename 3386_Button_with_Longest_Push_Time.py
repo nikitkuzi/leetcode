ec4a8becc96039c75190c0472a560b061fbea598
nikitkuzi <nikitkuzi@gmail.com>
2024-12-14 23:02:57 -0500

    initial commit

--- 3386_Button_with_Longest_Push_Time.py
+++ 3386_Button_with_Longest_Push_Time.py
@@ -1,11 +1,11 @@
 class Solution:
     def buttonWithLongestTime(self, events: List[List[int]]) -> int:
-        res = [events[0][0]]
+        res = events[0][0]
         diff = events[0][1]
         for i in range(1, len(events)):
             if diff < events[i][1]-events[i-1][1]:
                 diff = events[i][1]-events[i-1][1]
-                res = [events[i][0]]
+                res = events[i][0]
             if diff == events[i][1]-events[i-1][1]:
-                res.append(events[i][0])
-        return min(res)
+                res = min(res, events[i][0])
+        return res

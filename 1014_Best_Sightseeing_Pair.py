a7d6d2da12d78b7cb40c65a86edb363d259b465c
nikitkuzi <nikitkuzi@gmail.com>
2024-12-29 17:59:38 -0500

    initial commit

--- 1014_Best_Sightseeing_Pair.py
+++ 1014_Best_Sightseeing_Pair.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        n = len(values)
-        dp = [0] * n
-        dp[0] = values[0]
-        for i in range(1, n):
-
+        mx = values[0]
+        res = 0
+        for i in range(1, len(values)):
+            res = max(res, mx+values[i]-i)
+            mx = max(mx, values[i]+i)
+        return res

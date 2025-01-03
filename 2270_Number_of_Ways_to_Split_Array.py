02d16127db310d5254760a88d46d11e5db1b05d3
nikitkuzi <nikitkuzi@gmail.com>
2025-01-02 21:14:42 -0500

    initial commit

--- 2270_Number_of_Ways_to_Split_Array.py
+++ 2270_Number_of_Ways_to_Split_Array.py
@@ -1,15 +1,26 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        pref = [0]*len(nums)
-        suf = [0]*len(nums)+[0]
-        for i in range(len(nums)):
-            pref[i] = pref[i-1] + nums[i]
-            suf[-i-2] = suf[-i-1]+nums[-i-1]
+        lSum = 0
+        rSum = sum(nums)
         res = 0
-        # print(pref)
-        # print(suf)
         for i in range(len(nums)-1):
-            # print(pref[i], suf[i+1])
-            if pref[i] >= suf[i+1]:
-                res+=1
+            lSum += nums[i]
+            rSum -= nums[i]
+            if lSum >= rSum:
+                res += 1
         return res
+# class Solution:
+#     def waysToSplitArray(self, nums: List[int]) -> int:
+#         pref = [0]*len(nums)
+#         suf = [0]*len(nums)+[0]
+#         for i in range(len(nums)):
+#             pref[i] = pref[i-1] + nums[i]
+#             suf[-i-2] = suf[-i-1]+nums[-i-1]
+#         res = 0
+#         # print(pref)
+#         # print(suf)
+#         for i in range(len(nums)-1):
+#             # print(pref[i], suf[i+1])
+#             if pref[i] >= suf[i+1]:
+#                 res+=1
+#         return res

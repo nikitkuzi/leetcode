bc36e618376aab70d22fb57892822f9f83f82a88
nikitkuzi <nikitkuzi@gmail.com>
2025-05-17 18:23:32 +0300

    initial commit

---
+++ 75_Sort_Colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        red, white, blue = 0, 1, 2
+        l, m, r = 0, 0, len(nums) - 1
+        while m <= r:
+            x = nums[m]
+            if x == red:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif x == white:
+                m += 1
+            else:
+                nums[m], nums[r] = nums[r], nums[m]
+                r -= 1
+
+

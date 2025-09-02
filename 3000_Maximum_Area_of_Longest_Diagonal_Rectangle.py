7eae4a22faa380a07bb9a19fd7af0022bec3627b
nikitkuzi <nikitkuzi@gmail.com>
2025-09-02 21:42:38 +0300

    initial commit

--- 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.py
+++ 3000_Maximum_Area_of_Longest_Diagonal_Rectangle.py
@@ -1,6 +1,8 @@
 class Solution:
-    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+    @test
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]], test=None) -> int:
         mx = 0
+
         ans = 0
         for x,y in dimensions:
             d = (x*x+y*y)**0.5
@@ -11,3 +13,4 @@ class Solution:
             elif d == mx:
                 ans = max(ans, x*y)
         return ans
+print("test")

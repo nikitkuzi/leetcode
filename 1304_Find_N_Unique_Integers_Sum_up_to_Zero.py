8238a0964efd917bd3c3328394edbb39bbe4bdce
nikitkuzi <nikitkuzi@gmail.com>
2025-09-07 21:49:10 +0300

    initial commit

--- 1304_Find_N_Unique_Integers_Sum_up_to_Zero.py
+++ 1304_Find_N_Unique_Integers_Sum_up_to_Zero.py
@@ -1,7 +1,5 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
-        if n == 1:
-            return [0]
-        if n%2 == 0:
-            return list(i for i in range(-n//2, n//2+1) if i != 0)
-        return list(i for i in range(-(n-1)//2, (n-1)//2+1))
+        if n % 2 == 0:
+            return list(i for i in range(-n // 2, n // 2 + 1) if i != 0)
+        return list(i for i in range(-(n - 1) // 2, (n - 1) // 2 + 1))

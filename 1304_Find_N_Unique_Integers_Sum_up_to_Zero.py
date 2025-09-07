73d35281e8e03ffedf9a4a34aa4f94f5cc19f2a0
nikitkuzi <nikitkuzi@gmail.com>
2025-09-07 21:48:36 +0300

    initial commit

--- 1304_Find_N_Unique_Integers_Sum_up_to_Zero.py
+++ 1304_Find_N_Unique_Integers_Sum_up_to_Zero.py
@@ -4,4 +4,4 @@ class Solution:
             return [0]
         if n%2 == 0:
             return list(i for i in range(-n//2, n//2+1) if i != 0)
-        return list(i for i in range(-(n-3)//2, (n-3)//2+1) if i!=0)+[-3234, 2000, 1234]
+        return list(i for i in range(-(n-1)//2, (n-1)//2+1))

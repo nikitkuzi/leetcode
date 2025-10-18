422bc4e611e2f82ce52c87bf459613a74a44e5b8
nikitkuzi <nikitkuzi@gmail.com>
2025-10-18 13:23:15 +0300

    initial commit

---
+++ 3397_Maximum_Number_of_Distinct_Elements_After_Operations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxDistinctElements(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        cnt = 0
+        prev = -inf
+        for num in nums:
+            curr = min(max(num - k, prev + 1), num + k)
+            if curr > prev:
+                cnt += 1
+                prev = curr
+
+        return cnt
